89b1d5f54fbf2d8904020ee513e1e5b9555efc31
Jan <j.f.akerman@gmail.com>
2019-02-17 20:19:49 +0000

    Apps talking

--- src/main/java/com/janakerman/keycloakjwt/controller/MyController.java
+++ src/main/java/com/janakerman/keycloakjwt/controller/MyController.java
@@ -1,14 +1,19 @@
 package com.janakerman.keycloakjwt.controller;
 
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
+@ConditionalOnProperty(name = "keycloakjwt.mode", havingValue = "server")
 public class MyController {
 
     @GetMapping("/greeting")
+    @ResponseBody
     public String greeting() {
-        return "greeting";
+        System.out.println("received");
+        return "hi";
     }
 
 }
--- src/main/java/com/janakerman/keycloakjwt/service/RequestService.java
+++ src/main/java/com/janakerman/keycloakjwt/service/RequestService.java
@@ -1,14 +1,23 @@
 package com.janakerman.keycloakjwt.service;
 
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.http.ResponseEntity;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
 
 @Component
+@ConditionalOnProperty(name = "keycloakjwt.mode", havingValue = "client")
 public class RequestService {
 
     @Scheduled(fixedDelay = 1000)
     public void makeRequest() {
-        System.out.println("asdf");
+        System.out.println("requesting");
+
+        RestTemplate restTemplate = new RestTemplate();
+        ResponseEntity<String> response = restTemplate.getForEntity("http://server:8080/greeting", String.class);
+
+        System.out.println(response);
     }
 
 }
